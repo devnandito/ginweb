9a9fd3a3baeba99695557d6492ef92d0f239d8cc
devnandito <fhersa@gmail.com>
2023-04-25 18:55:52 -0400

    add func template

--- src/main.go
+++ src/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"html/template"
+	"strings"
+
 	"github.com/devnandito/ginweb/handlers"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	router.SetFuncMap(template.FuncMap{
+		"upper": strings.ToUpper,
+	})
 	router.Static("/assets", "./assets")
-	router.LoadHTMLGlob("templates/**/*")
+	router.LoadHTMLGlob("templates/*.tmpl")
 	router.GET("/modules/show", handlers.HandlerShowModule)
 	router.Run(":8080")
 }
